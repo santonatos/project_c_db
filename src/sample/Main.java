0f9022a5235db2b2521b8cd05978187c27205cda
Evan Duffy <elduffy@wpi.edu>
2017-03-25 14:38:07 -0400

    Added Derby Code From Example

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,87 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.sql.*;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+/**
+ * Created by evan on 3/23/17.
+ */
+public class Main extends Application{
+    static Connection conn = null;
+    static Statement stmt = null;
+
+    /**
+     * Program main
+     * @param args Command Line Args
+     */
+    public static void main(String[] args){
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        }
+        catch (ClassNotFoundException e){
+            System.out.println("CLASS NOT FOUND, CHECK DRIVER");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Driver good");
+        try {
+            conn = DriverManager.getConnection("jdbc:derby:evanDB;create=true");
+        }
+        catch (SQLException e){
+            System.out.println("Connection Error");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Connection Good!");
+
+        Application.launch(args);
+    }
+
+    /**
+     * Sends a command to the server and returns the result set
+     * @param command The command to send to the server
+     * @return
+     */
+    public static ResultSet sendCommand(String command){
+        ResultSet rs;
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(command);
+            return rs;
+
+        }
+        catch (SQLException e){
+            System.out.println("Error Querying, Trying Execute...");
+            try {
+                stmt.execute(command);
+                System.out.println("Executed Successfully");
+
+            }
+            catch (SQLException e2){
+                e.printStackTrace();
+                System.out.println("Query Error!");
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Start the javafx window, load from fxml
+     * @param stage Used by JFX
+     * @throws Exception
+     */
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("evandb.fxml"));
+
+        Scene scene = new Scene(root, 600, 150);
+
+        stage.setTitle("JDBC Tester");
+        stage.setScene(scene);
+        stage.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
