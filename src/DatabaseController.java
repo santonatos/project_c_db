a9dcc641fdf1fdc51aae9253fb7674c5ecade96d
Evan Duffy <elduffy@wpi.edu>
2017-03-25 15:45:04 -0400

    Added Method Skeletons to DB Controller Classes

--- src/DatabaseController.java
+++ src/DatabaseController.java
@@ -2,4 +2,20 @@
  * Created by evan on 3/25/17.
  */
 public class DatabaseController {
+    String url = "";
+    public DatabaseController (String _url){
+        this.url = _url;
+    }
+
+    public boolean send_Command(String command){
+        return false;
+    }
+
+    public boolean registerDriver(){
+        return false;
+    }
+
+    public boolean connect(){
+        return false;
+    }
 }
--- src/DatabaseEditor.java
+++ src/DatabaseEditor.java
@@ -2,4 +2,38 @@
  * Created by evan on 3/25/17.
  */
 public class DatabaseEditor {
+    DatabaseController dbc = null;
+    public DatabaseEditor (DatabaseController _dbc){
+        this.dbc = _dbc;
+    }
+
+    public boolean edit_Room(){
+        return false;
+    }
+    public boolean add_Room(){
+        return false;
+    }
+    public boolean remove_Room(){
+        return false;
+    }
+
+
+    public boolean addService(){
+        return false;
+    }
+    public boolean edit_Service(){
+        return false;
+    }
+    public boolean removeService(){
+        return false;
+    }
+
+
+    public boolean add_Room_Service(){
+        return false;
+    }
+    public boolean remove_Room_Service(){
+        return false;
+    }
+
 }
