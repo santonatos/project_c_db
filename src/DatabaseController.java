599666f7109c0a419f9cf815a34b0469f208d60c
Evan Duffy <elduffy@wpi.edu>
2017-03-25 20:32:06 -0400

    Populated Functions For DatabaseController Object And Instantiated It In main() To Test. Also Cleaned Up A Few Warnings

--- src/DatabaseController.java
+++ src/DatabaseController.java
@@ -1,21 +1,55 @@
+import java.sql.*;
+
 /**
  * Created by evan on 3/25/17.
+ * This Object will register, connect, and submit queries to any database specified by '_url' in the constructor
  */
 public class DatabaseController {
-    String url = "";
-    public DatabaseController (String _url){
+    private Connection conn = null;
+    private Statement stmt = null;
+    private String url = "";
+
+    DatabaseController(String _url){
         this.url = _url;
     }
 
-    public boolean send_Command(String command){
-        return false;
+    public ResultSet send_Command(String command){
+        ResultSet rs;
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(command);
+            return rs;
+
+        }
+        catch (SQLException e){
+            System.out.println("Error Querying, Trying Execute...");
+            try {
+                stmt.execute(command);
+                System.out.println("Executed Successfully");
+
+            }
+            catch (SQLException e2){
+                e.printStackTrace();
+                System.out.println("Query Error!");
+            }
+        }
+        return null;
     }
 
-    public boolean registerDriver(){
-        return false;
+    boolean registerDriver() throws ClassNotFoundException{
+        Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        return true;
     }
 
-    public boolean connect(){
-        return false;
+    boolean connect() throws SQLException{
+        conn = DriverManager.getConnection("jdbc:derby:testDB;create=true");
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        if (url.length() > 0)
+            return "DatabaseController connected to " + url;
+        return "Unconnected DatabaseController";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,59 +2,21 @@ import java.sql.*;
 
 /**
  * Created by evan on 3/25/17.
+ * Main Execution Entry Point
  */
 public class Main {
-    static Connection conn = null;
-    static Statement stmt = null;
+    private static DatabaseController dbc = null;
 
     public static void main(String[] args){
+        dbc = new DatabaseController("jdbc:derby:testDB;create=true");
         try {
-            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            dbc.registerDriver();
+            dbc.connect();
         }
-        catch (ClassNotFoundException e){
-            System.out.println("CLASS NOT FOUND, CHECK DRIVER");
+        catch (Exception e){
             e.printStackTrace();
-            return;
         }
-        System.out.println("Driver good");
-        try {
-            conn = DriverManager.getConnection("jdbc:derby:testDB;create=true");
-        }
-        catch (SQLException e){
-            System.out.println("Connection Error");
-            e.printStackTrace();
-            return;
-        }
-        System.out.println("Connection Good!");
-    }
-
-
-    /**
-     * Sends a command to the server and returns the result set
-     * @param command The command to send to the server
-     * @return
-     */
-    public static ResultSet sendCommand(String command){
-        ResultSet rs;
-        try {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(command);
-            return rs;
-
-        }
-        catch (SQLException e){
-            System.out.println("Error Querying, Trying Execute...");
-            try {
-                stmt.execute(command);
-                System.out.println("Executed Successfully");
-
-            }
-            catch (SQLException e2){
-                e.printStackTrace();
-                System.out.println("Query Error!");
-            }
-        }
-        return null;
+        System.out.println(dbc);
     }
 
 }
